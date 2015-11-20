4ae2f5d3956e421019043367db9ccfd8763b294f
mabrm9 <mabrm9@mail.missouri.edu>
2015-11-20 00:46:40 -0600

    Added hackweek project files

---
+++ hackweektest/main.js
@@ -0,0 +1,42 @@
+
+TweenMax.to(".botcirc",2, {
+x:500,
+opacity:100,
+});
+
+TweenMax.to(".mid",2, {
+x:500,
+opacity:100,
+delay:2
+});
+
+TweenMax.to(".head",2, {
+x:500,
+opacity:100,
+delay:8
+});
+
+TweenMax.to(".face",3, {
+opacity:100,
+delay:10
+});
+
+TweenMax.to(".hat",4, {
+opacity:100,
+delay:12
+});
+
+TweenMax.to(".botbut",2, {
+opacity:100,
+delay:4
+});
+
+TweenMax.to(".midbut",2, {
+opacity:100,
+delay:5
+});
+
+TweenMax.to(".topbut",2, {
+opacity:100,
+delay:6
+});
